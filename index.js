fa2ade32642fc7d43b6b84330e35c3522789e936
kamisch <chengtwv2@gmail.com>
2019-05-18 13:13:10 -0400

    setup js

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    var distance = "meters";
+    var hourVal = 0;
+    var minVal = 0;
+    var secVal = 0;
+
+    var outputData = function (){
+
+    }
+
+    var calculateSplits = function (){
+
+    }
+    $('#calculate').click(function() {
+
+      alert("Hello");
+    });
+    $(".dropdown-menu a").click( function() {
+        distance = $(this).text();
+    });
+  }); 
+
